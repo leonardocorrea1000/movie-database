1aa2e456eff003bd7b8a0ea3a8ad9c01c6a3ac53
Leonardo Correa <leonardo.correa@providesolution.com.br>
2019-12-11 21:09:37 -0300

    API: web requests

--- MovieDatabase/services/APIBase.swift
+++ MovieDatabase/services/APIBase.swift
@@ -8,11 +8,13 @@
 
 import Foundation
 
-class API {
+class APIBase {
     static let baseUrl = "https://api.themoviedb.org/3"
+    static let imageUrl = "https://image.tmdb.org/t/p/w200"
     static let apiKey = "c5850ed73901b8d268d0898a8a9d8bff"
     static let keyQuery = "?api_key=\(apiKey)"
     
-    static let upcomingMovies = "/movies/upcoming"
+    static let upcomingMovies = "/movie/upcoming"
+    static let genresList = "/genre/movie/list"
 
 }
--- MovieDatabase/services/MoviesService.swift
+++ MovieDatabase/services/MoviesService.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import Alamofire
+
+class MoviesService: NSObject {
+    static let sharedInstance = MoviesService()
+    
+    
+    func fetchUpcomingMovies(page: Int, completion: @escaping (_ movies: Movies?)->()) {
+        let url = APIBase.baseUrl + APIBase.upcomingMovies + APIBase.keyQuery + "&page=\(page)"
+        AF.request(url).responseJSON { response in
+            if response.error != nil {
+                completion(nil)
+                return
+            }
+            
+            let fetchResponse = try! JSONDecoder().decode(Movies.self, from: response.data!)
+            completion(fetchResponse)
+        }
+    }
+    
+    func fetchGenres(completion: @escaping (_ genres: Genres?) -> ()) {
+        AF.request(APIBase.baseUrl + APIBase.genresList + APIBase.keyQuery).responseJSON { response in
+            if response.error != nil {
+                completion(nil)
+                return
+            }
+            
+            let fetchResponse = try! JSONDecoder().decode(Genres.self, from: response.data!)
+            completion(fetchResponse)
+        }
+    }
+}
