03cd610783367f890b9f3c388bd3472930fb15cf
Leonardo Correa <leonardo.correa@providesolution.com.br>
2019-12-11 21:09:20 -0300

    Extensions

--- MovieDatabase/extensions/DateExtensions.swift
+++ MovieDatabase/extensions/DateExtensions.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+
+extension Date {
+    
+    
+    init(string: String) {
+        guard string != "" && string.count > 0 else {
+            self = Date()
+            return
+        }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.locale = Locale(identifier: "en_US")
+        self = dateFormatter.date(from: string)!
+    }
+    
+    func toDDMMYYY() -> String {
+        return self.toFormat("dd/MM/yyyy")
+    }
+    
+    func toFormat(_ format: String, locale: Locale = NSLocale(localeIdentifier: "pt_BR") as Locale) -> String {
+        let currentDate = self
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = format
+        dateFormatter.locale = locale
+        
+        let convertedDate: String = dateFormatter.string(from: currentDate)
+        return convertedDate
+    }
+    
+}
